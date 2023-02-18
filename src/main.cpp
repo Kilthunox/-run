bf6caaea57327717927ec852cc485da60dba66f6
Kalen <kalenwillits@gmail.com>
2023-02-18 14:29:45 -0500

    FEEATURE: Constrained scripts to .sh files only & changed config directory to "run".

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 
 const std::filesystem::path HOME_DIR {std::string("/home/") + std::getenv("USER")};
-const std::string CONFIG_DIR {".fn/"};
+const std::string CONFIG_DIR {"run/"};
 std::map<std::string, std::string> funcs {};
 
 
@@ -23,16 +23,23 @@ std::string read_file(const std::string &file_path) {
 
 
 std::string get_filename(const std::string &file_path) {
-    std::string::size_type const filename(file_path.find_last_of('/'));
-	std::string filename_without_extension = file_path.substr(filename + 1);
+    std::string::size_type const filename_index {file_path.find_last_of('/')};
+	std::string filename = file_path.substr(filename_index + 1);
+	std::string filename_without_extension = filename.substr(0, filename.find_last_of('.'));
 	return filename_without_extension;
+}
+
+std::string get_file_extension(const std::string &file_path) {
+	return file_path.substr(file_path.find_last_of('.') + 1);
 }
 
 
 void load_funcs(std::filesystem::path dir) {
 	if (std::filesystem::exists(dir / CONFIG_DIR)) {
 		for (auto const &file: std::filesystem::recursive_directory_iterator{dir / CONFIG_DIR}) {
-			funcs[get_filename(file.path().string())] = file.path().string();
+			if (get_file_extension(file.path().string()) == "sh") { 
+				funcs[get_filename(file.path().string())] = file.path().string();
+			}
 		}
 	}
 
