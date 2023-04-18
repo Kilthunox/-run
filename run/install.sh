c6f15f16b80d2a4623a36996a39fd88b6b312dae
Kalen <kalenwillits@gmail.com>
2023-04-18 17:16:42 -0400

    Fixing install path on script.

--- run/install.sh
+++ run/install.sh
@@ -3,7 +3,7 @@
 set -xe
 g++ src/*.cpp -std=c++17 -o fn
 mv fn ~/Applications/fn/fn
-echo "export PATH=$PATH:~/Applications/fn/" >> .bashrc
+echo "export PATH=$PATH:~/Applications/fn/" >> ~/.bashrc
 
 
 
